2437486286440726e41f26c63a1c9fde7a53f892
Brody Roche <rochebrody4@gmail.com>
2025-12-16 23:06:36 -0600

    Finished service discover and gateway

--- gateway/src/main/java/com/example/gateway/GatewayApp.java
+++ gateway/src/main/java/com/example/gateway/GatewayApp.java
@@ -1,17 +1,13 @@
-package com.revature.lab;
+package com.example.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaServer;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableEurekaServer
+@EnableDiscoveryClient
 public class GatewayApp {
-
     public static void main(String[] args) {
         SpringApplication.run(GatewayApp.class, args);
     }
-
-    // Note: Routes are usually defined in application.yml,
-    // but can be defined via @Bean RouteLocatorBuilder if preferred.
 }
